aca0f3d3a4242aab785788965130ccb911ceed5b
6suhyeon <dbtngus20@naver.com>
2025-07-31 20:54:03 +0900

    feat : problem4

---
+++ coding/src/Problem4.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Problem4 {
+    public StringBuilder solution(String str) {
+        StringBuilder answer = new StringBuilder();
+
+        for(int i = str.length()-1; i>=0; i--) {
+            answer.append(str.charAt(i));
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Problem4 p4 = new Problem4();
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        List<String> list = new ArrayList<>();
+
+        for(int i=0; i<num; i++) {
+            list.add(sc.next());
+        }
+
+        for(int i=0; i<num; i++) {
+            System.out.println(p4.solution(list.get(i)));
+        }
+    }
+}
