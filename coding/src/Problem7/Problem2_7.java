9e78cdcf0986bed724f35fffad5ee8259ca1d96a
6suhyeon <dbtngus20@naver.com>
2025-09-05 14:55:25 +0900

    feat : Problem2_7

---
+++ coding/src/Problem7/Problem2_7.java
@@ -0,0 +1,39 @@
+package Problem7;
+
+import java.util.Scanner;
+
+public class Problem2_7 {
+    public int solution(int n, int[] arr) {
+        int result = 0;
+        int target = 0;
+
+        for(int i = 0; i<n; i++) {
+            if(arr[i] == 0) {
+                target = 0;
+                result += target;
+            }
+
+            else {
+                target++;
+                result += target;
+            }
+        }
+
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        Problem2_7 p7 = new Problem2_7();
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print(p7.solution(n, arr));
+    }
+}
