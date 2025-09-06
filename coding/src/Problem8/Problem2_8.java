526f5c6278dedc5ed71d8f8d8de2e42a2676dc6b
6suhyeon <dbtngus20@naver.com>
2025-09-06 16:39:35 +0900

    feat : Problem2_8

---
+++ coding/src/Problem8/Problem2_8.java
@@ -0,0 +1,39 @@
+package Problem8;
+
+import java.util.Scanner;
+
+public class Problem2_8 {
+    public int[] solution(int n, int[] arr) {
+        int cnt = 1;
+        int[] result = new int[n];
+
+        for(int i=0; i<n; i++) {
+
+            for(int j=0; j<n; j++) {
+                if(arr[i] < arr[j]) {
+                    cnt++;
+                }
+            }
+            result[i] = cnt;
+            cnt = 1;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Problem2_8 p8 = new Problem2_8();
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for(int result : p8.solution(n, arr)) {
+            System.out.print(result + " ");
+        }
+    }
+}
