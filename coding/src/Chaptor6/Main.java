5505e278603079da0c5f2bf92acadfeffb056a56
6suhyeon <dbtngus20@naver.com>
2025-12-28 16:11:47 +0900

    feat : chaptor6 - 올바른 괄호

---
+++ coding/src/Chaptor6/Main.java
@@ -0,0 +1,31 @@
+package Chaptor6;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public String solution(String s) {
+        String answer = "YES";
+        Stack<Character> stack = new Stack<>();
+
+        for (char x : s.toCharArray()) {
+            if(x=='(') stack.push(x);
+            else {
+                if(stack.isEmpty()) return "NO";
+                stack.pop();
+            }
+        }
+
+        if(!stack.isEmpty()) return "NO";
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main p1 = new Main();
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+        System.out.println(p1.solution(s));
+    }
+}
