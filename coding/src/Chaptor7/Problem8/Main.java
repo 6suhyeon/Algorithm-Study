5e9701e71fdacb5dfb4ec7b5a59b9e7b20621723
6suhyeon <dbtngus20@naver.com>
2026-01-01 18:57:24 +0900

    feat : chaptor7 - 이분검색

---
+++ coding/src/Chaptor7/Problem8/Main.java
@@ -0,0 +1,40 @@
+package Chaptor7.Problem8;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+        int answer = 0;
+        int lt = 0;
+        int rt = n-1;
+
+        while (lt <= rt) {
+            int mid = (lt+rt) / 2;
+            if(arr[mid] == m) {
+                answer = mid+1;
+                break;
+            }
+            if(arr[mid] > m) {
+                rt = mid - 1;
+            }
+            else {
+                lt = mid + 1;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
